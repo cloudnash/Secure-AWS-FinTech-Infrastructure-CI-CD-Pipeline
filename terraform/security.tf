3bea517ccda901bd5a9e85cb3bb41c7d11d80bde
Nashit Ahmad <fusionmad47@gmail.com>
2026-06-16 19:18:02 +0530

    Create security.tf

---
+++ terraform/security.tf
@@ -0,0 +1,44 @@
+# Security Group for ECS App (Only allows port 8080)
+resource "aws_security_group" "app_sg" {
+  name        = "fintech-app-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.fintech_vpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# IAM Role for ECS Task Execution (RBAC Concept)
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "fintech-ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
